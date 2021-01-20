10dab0207dbe00da32e0cad0089d4ce1ee7c10fc
Issac To <issacto606@gmail.com>
2021-01-20 14:41:30 +0000

    Heroku Config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const reviews = require("./routes/api/reviews");
 const app = express();
-// Bodyparser middleware
+const path = require('path');// Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
     extended: false
@@ -31,5 +31,16 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/reviews", reviews);
+
+
+
+//Serve static asset if in production
+if(process.env.NODE_ENV ==='production'){
+  app.use(express.static('client/build'));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  });
+
+}
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
