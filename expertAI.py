62984713b3569d99b93e4ccd7f08c4bd3cfc350c
Issac To <issacto606@gmail.com>
2021-01-25 22:27:15 +0000

    Add AWS Lambda to expertAI.py

--- expertAI.py
+++ expertAI.py
@@ -1,16 +1,18 @@
-
+#mv expertAI.py ExperAi/lib/python3/site-packages/
 import os
-import sys
 os.environ["EAI_USERNAME"] = 'tototototoman@gmail.com'
 os.environ["EAI_PASSWORD"] = 'pUfver-rodkoj-zigso7'
 from expertai.nlapi.cloud.client import ExpertAiClient
-client = ExpertAiClient()
-text = sys.argv[0] 
-language= 'en'
 
-document = client.specific_resource_analysis(
-    body={"document": {"text": text}}, 
-    params={'language': language, 'resource': 'sentiment'})
+def lambda_handler(event, context):
+    client = ExpertAiClient()
+    text = event["review"]
+    language= 'en'
+    document = client.specific_resource_analysis(
+        body={"document": {"text": text}}, 
+        params={'language': language, 'resource': 'sentiment'})
 
-print("sentiment:", document.sentiment.overall)
-sys.stdout.flush()
+    return {
+        'statusCode': 200,
+        'body': json.dumps(document.sentiment.overall)
+    }
