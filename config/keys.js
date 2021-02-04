6bfc116e521e0a3ff163d3d7ddb4df03c9af01e0
Issac To <issacto606@gmail.com>
2021-02-04 01:55:48 +0000

    Hide

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://issac:XqRw1VouxsqfBWae@merntry.xitkw.mongodb.net/MernTry?retryWrites=true&w=majority",
+    useUnifiedTopology: true ,
+    secretOrKey: "secret"
+};
